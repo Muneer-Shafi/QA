c30fe41acc16ce90dea4b764efbe3a223fcd23e7
Muneer shafi <zmzahidmuneer@gmail.com>
2022-06-18 12:53:55 +0530

    Delete employee logic pending

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -7,7 +7,7 @@ import { EmployeeType } from './types';
 })
 export class EmployeeComponent implements OnInit {
   constructor(private employeeApi: EmployeeService) {}
-  public isSingleEmployee:boolean =false;
+  public isSingleEmployee: boolean = false;
   public employeeList: EmployeeType[] = [];
   public singleEmployee: EmployeeType = {
     id: 0,
@@ -28,9 +28,16 @@ export class EmployeeComponent implements OnInit {
       this.singleEmployee = res;
     });
 
-    if(this.singleEmployee.id != 0){
+    if (this.singleEmployee.id != 0) {
       this.isSingleEmployee = true;
     }
     return this.singleEmployee;
+  }
+
+  deleteEmployee(id: number): boolean {
+
+    
+    //logic here
+    return true;
   }
 }
